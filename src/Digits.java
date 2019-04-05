b185f436fe3dce996abc0f9528941748e8b2a5c0
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2019-04-04 20:43:47 -0400

    digits

--- src/Digits.java
+++ src/Digits.java
@@ -11,10 +11,35 @@ public class Digits
     public Digits (int num)
     {
         //implemented in part (a)
+        digitList = new ArrayList<Integer>();
+        if(num>0){
+            int remainder =0;
+            while(num>0){
+                remainder = num % 10;
+                num = num/10;
+                digitList.add(0, remainder);
+            }
+        }
+        else if(num==0){
+            digitList.add(0);
+        }
+
+        //String stringNum = String.valueOf(num);
+        //if(num>=0) {
+            //for (int i = 0; i < stringNum.length(); i++) {
+                //digitList.add(Integer.valueOf(stringNum.substring(i, i+1)));
+            //}
+        //}
     }
     public boolean isStrictlyIncreasing()
     {
         //implemented in part (b)
+        for(int i=0; i<digitList.size()-1; i++){
+            if(digitList.get(i)>= digitList.get(i+1)){
+                return false;
+            }
+        }
+        return true;
     }
     //Extra method to make the runner easier to read
     public ArrayList<Integer> getDigitList()
